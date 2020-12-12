a646c10933fca5e1a7b38584a54ea43ecd27e279
Njugi Mwaura <nmsteve45@gmail.com>
2020-12-12 14:09:35 +0300

    file added via upload

---
+++ va.pl
@@ -0,0 +1,45 @@
+suggestFruit(Fruit) :- write('Do your eat high-acidic foods?: '),read(Acidic),write('Which vitamin do you want?: '),read(Vitamin), fruit(Fruit,Acidic,Vitamin).
+suggestVegetable(Vegetable) :- write('Do your eat high-acidic foods?: '),read(Acidic),write('Which vitamin do you want?: '),read(Vitamin), vegetable(Vegetable,Acidic,Vitamin).
+
+
+%fruits
+fruit(cantaloupe,Acidic,Vitamin)   :- Acidic = yes ,Vitamin = a.
+fruit(apricots,Acidic,Vitamin)     :- Acidic = yes ,Vitamin = a.
+fruit(garapefruit,Acidic,Vitamin)   :- Acidic = yes ,Vitamin = a.
+
+fruit(squash,Acidic,Vitamin)       :- Acidic = no ,Vitamin = a.
+fruit(mango,Acidic,Vitamin)        :- Acidic = no ,(Vitamin = a ; Vitamin = e).
+fruit(watermelon,Acidic,Vitamin)   :- Acidic = no ,Vitamin = a.
+
+fruit('berries - Strawberries,Bilberries,Cranberries. ',Acidic,Vitamin)  :- Acidic = yes ,Vitamin = c.
+fruit('citrus fruits - oranges,limes and lemons',Acidic,Vitamin)         :- Acidic = yes ,Vitamin = c.
+fruit(kiwifruit,Acidic,Vitamin)   :- Acidic = yes ,(Vitamin = a ; Vitamin = e).
+
+fruit(papaya, Acidic,Vitamin)      :- Acidic = no ,Vitamin = c.
+
+fruit( avocado, Acidic,Vitamin)      :- Acidic = no ,Vitamin = e.
+
+%vegetables
+vegetable('Sukuma wiki',Acidic,Vitamin) :- Acidic = yes ,(Vitamin = a ; Vitamin = k).
+vegetable('Chili Pepper, Hot',Acidic,Vitamin) :- Acidic = yes ,Vitamin = a.
+vegetable('Pepper, Le Rouge Royale',Acidic,Vitamin) :- Acidic = yes ,Vitamin = a.
+vegetable( tomato,Acidic,Vitamin) :- Acidic = yes ,(Vitamin = a; Vitamin = c).
+
+vegetable(carrots,Acidic,Vitamin)            :- Acidic = no ,Vitamin = a.
+vegetable('Lettuce, Leafs',Acidic,Vitamin)   :- Acidic = no ,Vitamin = a.
+vegetable('Lettuce, Romaine',Acidic,Vitamin) :- Acidic = no ,Vitamin = a.
+
+
+vegetable('mustard greens',Acidic,Vitamin) :- Acidic = yes ,(Vitamin = c ;Vitamin = k).
+vegetable('bell peppers',Acidic,Vitamin)   :- Acidic = yes ,Vitamin = c.
+
+vegetable(broccoli,Acidic,Vitamin)           :- Acidic = no ,( Vitamin = c ; Vitamin = k).
+vegetable(sprouts,Acidic,Vitamin)            :- Acidic = no ,( Vitamin = c ; Vitamin =k).
+
+vegetable(mushrooms,Acidic,Vitamin)          :- Acidic = no ,Vitamin = d.
+
+vegetable('Turnip Greens (raw)',Acidic,Vitamin)          :- Acidic = no ,Vitamin = e.
+
+vegetable(spinach,Acidic,Vitamin) :- Acidic = no ,(Vitamin = a ; Vitamin = e ; vitamin = k).
+
+
